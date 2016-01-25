2d1e34c4a7c5e3c3845a5c23966fc3cc7e61cbf0
twfx7758@163.com <twfx7758@163.com>
2016-01-25 09:16:16 +0800

    https://github.com/twfx7758/LearnDemo.git

---
+++ Learn.SDK/SimpleSpinLock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Learn.SDK
+{
+    public class SimpleSpinLock{
+        private Int32 m_ResourceInUse;
+
+        public void Enter() {
+            //将资源设置为“正在使用”（1），如果这个线程是把它从“自由使用”（0）
+            //变成“正在使用”（1），就返回，以便执行Enter()调用之后的代码
+            while (Interlocked.Exchange(ref m_ResourceInUse, 1) != 0) { 
+                /*Black Magic*/
+            }
+        }
+
+        public void Leave() { 
+            //将资源标志为“自由使用”(0)
+            Thread.VolatileWrite(ref m_ResourceInUse, 0);
+        }
+    }
+}
---
+++ Learn.SDK/SimpleWaitLock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Learn.SDK
+{
+    public class SimpleWaitLock : IDisposable
+    {
+        private AutoResetEvent m_ResourceFree = new AutoResetEvent(true);//最初可以自由使用
+
+        public void Enter() {
+            //在内核中阻塞，等待资源，然后返回
+            m_ResourceFree.WaitOne();
+        }
+
+
+        public void Leave() {
+            m_ResourceFree.Set();//将资源标记为“自由使用”(Free)
+        }
+
+        public void Dispose() {
+            m_ResourceFree.Dispose();
+        }
+    }
+}
