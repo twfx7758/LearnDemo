93e9fe69e0330423b9c384198e51bc3ffe165439
twfx7758@163.com <twfx7758@163.com>
2016-01-27 16:41:02 +0800

    https://github.com/twfx7758/LearnDemo.git

---
+++ Learn.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Learn.Algorithm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn.ConsolePlat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] x = { 6, 2, 4, 1, 5, 9 };
+            BaseAlgorithm.Bubble_Sort(x);
+            foreach (var item in x)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
