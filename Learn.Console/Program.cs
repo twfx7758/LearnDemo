86506e0ec4bd7d3457fa69bcf48e1989d2118237
twfx7758@163.com <twfx7758@163.com>
2016-02-03 10:39:28 +0800

    未更新内容

--- Learn.Console/Program.cs
+++ Learn.Console/Program.cs
@@ -11,13 +11,19 @@ namespace Learn.ConsolePlat
     {
         static void Main(string[] args)
         {
+            
+            Console.ReadLine();
+        }
+
+        //冒泡算法测试
+        static void TestMethod()
+        {
             int[] x = { 6, 2, 4, 1, 5, 9 };
             BaseAlgorithm.Bubble_Sort(x);
             foreach (var item in x)
             {
                 Console.WriteLine(item);
             }
-            Console.ReadLine();
         }
     }
 }
