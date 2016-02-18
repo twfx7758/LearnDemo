9a0ade5a01f259515f73820e2b2e0c1e2f436edd
twfx7758@163.com <twfx7758@163.com>
2016-02-18 15:39:25 +0800

    www

--- WCF.Hosting/Program.cs
+++ WCF.Hosting/Program.cs
@@ -14,13 +14,27 @@ namespace WCF.Hosting
     {
         static void Main(string[] args)
         {
-            using (ServiceHost host = new ServiceHost(typeof(CalculatorService))) {
+            HostByBaseAddress();
+        }
+
+        /// <summary>
+        /// 基地址+相对地址
+        /// </summary>
+        static void HostByBaseAddress()
+        {
+            Uri[] baseAddress = new Uri[2];
+            baseAddress[0] = new Uri("http://127.0.0.1/myservices");
+            baseAddress[1] = new Uri("net.tcp://127.0.0.1/myservices");
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService), baseAddress))
+            {
                 //在配置里添加Behaviors
-                //host.AddServiceEndpoint(typeof(ICalculate), new WSHttpBinding(), "http://127.0.0.1:8888/calculatorservice");
-                //if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null) {
+                host.AddServiceEndpoint(typeof(ICalculate), new WSHttpBinding(), "calculatorservice");
+                host.AddServiceEndpoint(typeof(ICalculate), new NetTcpBinding(), "calculatorservice");
+                //if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+                //{
                 //    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
                 //    behavior.HttpGetEnabled = true;
-                //    behavior.HttpGetUrl = new Uri("http://127.0.0.1:8888/calculatorservice");
+                //    behavior.HttpGetUrl = new Uri("http://127.0.0.1/calculatorservice");
                 //    host.Description.Behaviors.Add(behavior);
                 //}
 
