0a7fe6f3f3c044e1c5c4adcbb2f7ccbe1843fae9
twfx7758@163.com <twfx7758@163.com>
2016-02-22 15:49:34 +0800

    Hash测试

--- Learn.Console/Program.cs
+++ Learn.Console/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Learn.Framework;
+using System.Collections;
 
 namespace Learn.ConsolePlat
 {
@@ -12,7 +14,35 @@ namespace Learn.ConsolePlat
         static void Main(string[] args)
         {
             
+
             Console.ReadLine();
+        }
+
+        //Hash示例
+        static void HashDemo()
+        {
+            HashTest test = new HashTest();
+            Dictionary<int, string> dic = test.DictionaryTst();
+            Console.WriteLine("=======Dictionary Test:=======");
+            foreach (int key in dic.Keys)
+            {
+                Console.WriteLine("Key:{0},Value:{1}", key, dic[key]);
+            }
+
+            Console.WriteLine("=======HashTable Test=======");
+            Hashtable ht = test.HashtableTest();
+            foreach (int key in ht.Keys)
+            {
+                Console.WriteLine("Key:{0},Value:{1}", key, ht[key]);
+            }
+
+
+            Console.WriteLine("=======HashSet Test=======");
+            HashSet<string> hs = test.HashSet();
+            foreach (string key in hs)
+            {
+                Console.WriteLine("Key:{0}", key);
+            }
         }
 
         //冒泡算法测试
