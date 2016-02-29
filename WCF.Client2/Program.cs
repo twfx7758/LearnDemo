fba24c5214e61d4dd2d899df3494da343aa24237
twfx7758@163.com <twfx7758@163.com>
2016-02-29 16:07:33 +0800

    WCF

--- WCF.Client2/Program.cs
+++ WCF.Client2/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WCF.Client2
 {
@@ -10,9 +13,37 @@ namespace WCF.Client2
     {
         static void Main(string[] args)
         {
-            new ServiceInvoker().Add(10, 12);
+            //new ServiceInvoker().Add(10, 12);
+
+            //发送消息
+            SendMessageClient();
 
             Console.ReadLine();
+        }
+
+        static void SendMessageClient()
+        {
+            Uri listUri = new Uri("http://127.0.0.1:3721/listener");
+            Binding binding = new BasicHttpBinding();
+            IChannelFactory<IRequestChannel> channelFactory = binding.BuildChannelFactory<IRequestChannel>();
+            channelFactory.Open();
+
+            //创建、开启请求信道
+            IRequestChannel channel = channelFactory.CreateChannel(new EndpointAddress(listUri));
+            channel.Open();
+
+            var message = channel.Request(CreateRequestMessage(binding));
+            Console.WriteLine(message);
+        }
+
+        static Message CreateRequestMessage(Binding binding)
+        {
+            string action = "http://www.kf.com/CalculatorService/AddResponse";
+            XNamespace ns = "http://www.kf.com";
+            XElement body = new XElement(new XElement(ns + "Add", 
+                new XElement(ns + "x", 1),
+                new XElement(ns + "y", 2)));
+            return Message.CreateMessage(binding.MessageVersion, action, body);
         }
     }
 }
