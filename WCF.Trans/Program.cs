80581615cc443e0e43c9a049603ea568b911e364
twfx7758@163.com <twfx7758@163.com>
2016-03-02 11:22:47 +0800

    WCF分布式事务

---
+++ WCF.Trans/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace WCF.Trans
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(new CommittableTransaction().TransactionInformation.LocalIdentifier);
+            Console.WriteLine(new CommittableTransaction().TransactionInformation.LocalIdentifier);
+            Console.WriteLine(new CommittableTransaction().TransactionInformation.LocalIdentifier);
+
+            Console.ReadLine();
+        }
+    }
+}
