9ae94ff8e9c5ae5fe0601e1d1a85dcf29dd8b43e
twfx7758@163.com <twfx7758@163.com>
2016-03-03 21:49:07 +0800

    WCF

---
+++ WCF.Service.Interface/IWASHostService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace WCF.Service.Interface
+{
+    [ServiceContract(Namespace = "http://www.kf.com/WASHost")]
+    public interface IWASHostService
+    {
+        [OperationContract]
+        string HelloWCF(string str);
+    }
+}
---
+++ WCF.Service/WASHostService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WCF.Service.Interface;
+
+namespace WCF.Service
+{
+    public class WASHostService : IWASHostService
+    {
+        public string HelloWCF(string str)
+        {
+            return string.Format("Get message from client is : {0}", str);
+        }
+    }
+}
