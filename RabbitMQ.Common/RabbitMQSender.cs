02050fbb8f8d61b39d1ba3045d0fa9293ec3b553
twfx7758@163.com <twfx7758@163.com>
2016-03-17 14:23:47 +0800

    RabbitMQ

--- RabbitMQ.Common/RabbitMQSender.cs
+++ RabbitMQ.Common/RabbitMQSender.cs
@@ -12,25 +12,28 @@ namespace RabbitMQ.Common
 
         //客户端发送消息的时候要标记上消息的持久化状态
         //可以在创建队列的时候设置此队列是持久化的，但是队列中的消息要在我们发送某个消息的时候打上需要持久化的状态标记。
-        public void TriggerEventMessage(EventMessage eventMessage, string exChange, string queue)
+        public void TriggerEventMessage(EventMessage eventMessage)
         {
             Context.SendConnection = RabbitMQClientFactory.CreateConnectionForSend();//获取连接
             using (Context.SendConnection)
             {
-                Context.SendChannel = RabbitMQClientFactory.CreateModel(Context.SendConnection);//获取通道
+                //获取发送通道
+                Context.SendChannel = RabbitMQClientFactory.CreateModel(Context.SendConnection);
 
                 const byte deliveryMode = 2;
 
                 using (Context.SendChannel)
                 {
-                    var messageSerializer = MessageSerializerFactory.CreateMessageSerializerInstance();//序列化消息器
+                    //序列化消息器
+                    var messageSerializer = MessageSerializerFactory.CreateMessageSerializerInstance();
 
+                    //消息持久化
                     var properties = Context.SendChannel.CreateBasicProperties();
-                    properties.DeliveryMode = deliveryMode; //消息持久化
+                    properties.DeliveryMode = deliveryMode; 
 
                     //推送消息
                     byte[] message = messageSerializer.SerializerBytes(eventMessage);
-                    Context.SendChannel.BasicPublish(exChange, queue, properties, message);
+                    Context.SendChannel.BasicPublish(Context.SendExchange, Context.SendQueueName, properties, message);
                 }
             }
         }
