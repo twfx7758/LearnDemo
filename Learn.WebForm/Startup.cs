28152d2351f00122d1950bf9d9e844306f87bded
屈文斌 <twfx7758@hotmail.com>
2016-03-23 16:16:20 +0800

    Commit

---
+++ Learn.WebForm/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Learn.WebForm.Startup))]
+namespace Learn.WebForm
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
