ce948d0c95b579f09c96af8bf03c1b1fb3432bd7
屈文斌 <twfx7758@hotmail.com>
2016-04-02 09:27:07 +0800

    AOP

---
+++ Common.AOP/Implementation/TransactionAOP.cs
@@ -0,0 +1,31 @@
+﻿using Common.AOP.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Common.AOP.Implementation
+{
+    public class TransactionAOP : IInterception
+    {
+        TransactionScope tran = null;
+
+        public void ExceptionHandle()
+        {
+            tran.Dispose();
+        }
+
+        public void PostInvoke()
+        {
+            tran.Complete();
+            tran.Dispose();
+        }
+
+        public void PreInvoke()
+        {
+            tran = new TransactionScope();
+        }
+    }
+}
