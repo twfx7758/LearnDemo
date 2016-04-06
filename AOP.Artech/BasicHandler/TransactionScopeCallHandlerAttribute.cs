0755672095477d21a9f59bc10ed3cf9674486e52
twfx7758@163.com <twfx7758@163.com>
2016-04-06 11:14:38 +0800

    AOP

---
+++ AOP.Artech/BasicHandler/TransactionScopeCallHandlerAttribute.cs
@@ -0,0 +1,18 @@
+﻿using AOP.Artech.Abstract;
+using AOP.Artech.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOP.Artech.BasicHandler
+{
+    public class TransactionScopeCallHandlerAttribute : HandlerAttribute
+    {
+        public override ICallHandler CreateCallHandler()
+        {
+            return new TransactionScopeCallHandler() { Ordinal = this.Ordinal, ReturnIfError = this.ReturnIfError };
+        }
+    }
+}
