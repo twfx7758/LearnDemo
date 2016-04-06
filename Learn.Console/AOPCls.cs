624ac0ef5cbf95f090ede077cd995644beabe02c
twfx7758@163.com <twfx7758@163.com>
2016-04-06 11:03:27 +0800

    AOP

--- Learn.Console/AOPCls.cs
+++ Learn.Console/AOPCls.cs
@@ -1,5 +1,4 @@
-﻿using Common.AOP.Attribute;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Learn.ConsolePlat
 {
-    [AOPProxyAttribute()]
     public class AOPCls : ContextBoundObject
     {
         public void MainMethod()
