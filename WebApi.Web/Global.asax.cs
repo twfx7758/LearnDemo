c110a4191fa47da1327631c143e90f2a059f11f3
twfx7758@163.com <twfx7758@163.com>
2016-04-07 11:23:18 +0800

    Web Api

---
+++ WebApi.Web/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using WebApi.Sdk;
+
+namespace WebApi.Web
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new HttpMethodOverrideHandler());
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
