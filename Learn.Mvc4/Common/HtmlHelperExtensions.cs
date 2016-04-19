70082b329d3f82d265347e1f83f7ec417669b57b
twfx7758@163.com <twfx7758@163.com>
2016-04-19 11:39:49 +0800

    Modify

---
+++ Learn.Mvc4/Common/HtmlHelperExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Learn.Mvc4.Common
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString ListViewAssemblies(this HtmlHelper helper)
+        {
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies().Where(
+                a => a.FullName.StartsWith("App_Web_")
+            ))
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.InnerHtml = assembly.FullName;
+                ul.InnerHtml += li.ToString();
+            }
+
+            return new MvcHtmlString(ul.ToString());
+        }
+    }
+}
---
+++ Learn.Mvc4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Learn.Mvc4.MvcApplication" Language="C#" %>
