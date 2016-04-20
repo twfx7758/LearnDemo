f5e6e71d2ab06c6849390a84f779d2d64202f6dd
twfx7758@163.com <twfx7758@163.com>
2016-04-20 09:46:41 +0800

    Console

---
+++ Learn.Mvc5/DateTimeFormatSettings.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn.Mvc5
+{
+    public class DateTimeFormatSettings
+    {
+        public string LongDatePattern { get; set; }
+        public string LongTimePattern { get; set; }
+        public string ShortDatePattern { get; set; }
+        public string ShortTimePattern { get; set; }
+
+        public DateTimeFormatSettings(IConfiguration configuration)
+        {
+            this.LongDatePattern = configuration["LongDatePattern"];
+            this.LongTimePattern = configuration["LongTimePattern"];
+            this.ShortDatePattern = configuration["ShortDatePattern"];
+            this.ShortTimePattern = configuration["ShortTimePattern"];
+        }
+    }
+}
--- Learn.Mvc5/Startup.cs
+++ Learn.Mvc5/Startup.cs
@@ -15,6 +15,7 @@ namespace Learn.Mvc5
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            IServiceProvider provider = services.BuildServiceProvider();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
